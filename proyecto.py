2eafcead6603f6d6fe04766ba0425fcefdebb209
jackeline-inturias <inturiasjackeline7@gmail.com>
2026-06-18 17:34:07 +0000

    agregue receta de las funciones buscador_barrios buscar_max y barrios5

--- proyecto.py
+++ proyecto.py
@@ -58,12 +58,42 @@ def Lectura_Archivo ():
 
 
 def buscador_barrio (alquiler):
+'''
+Representamos los alquileres mediante una lista de diccionarios, cada lista
+tiene informacion de un hospedaje. 
+Alquiler representa una lista de hospedajes.
+
+alquiler: list  
+
+buscador_barrio: list -> dict
+
+la clave es el nombre del barrio y el valor la cantidad de hospedajes
+ejemplo: 
+buscador_barrio([{"neighbourhood": "Western Addition"}, {"neighbourhood": "Nob Hill"}])
+=
+{"Western Addition": 2, "Nob Hill": 1}
+'''
        diccionario={}
        for linea in alquiler:
               diccionario[linea["neighbourhood"]]=diccionario.get(linea["neighbourhood"],0)+1
-       return diccionario       
+       return diccionario   
+
 #version con recursion
+
 def buscar_max(barrios,dicc_max={}):
+''' 
+Repreentamos con un diccionario la cantidad de hospedajes por barrio
+buscar_max: dict,dict ->dict
+barrios:dict
+dicc_max:dict     
+El parametro barrios representa un diccionario.
+La clave es el nombre del barrio y el valor la cantidad de hospedajes.
+
+El parametro dicc_max representa un diccionario que va almacenando los barrios con el maximo
+numero de hospedajes.
+
+La funcion retorna a un diccionario con los 5 barrios con mayor hospedajes.
+'''
        max=0
        claves = list(barrios.keys())
        for x in claves:
@@ -80,6 +110,15 @@ def buscar_max(barrios,dicc_max={}):
 
 #version con dos ciclos
 def barrios5 (barrios):
+'''
+Representamos con un diccionario la cantidad de hospedajes por barrios
+barrios: dict
+barrios5: dict-> dict
+el parametro representa un diccionario. la clave es el nombre del barrio 
+y el valor la cantidad de hospedajes del barrio.
+La funcion retorna a un diccionario con los 5 barrios con mayor hospedajes.
+ejemplo: 
+''' 
     lista_5 = {}
 
     while len(lista_5) < 5:
